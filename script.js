95cdad2d01a0f58be6ef2cef49df648be431f9a2
Prince kumar <princekr3006@gmail.com>
2024-10-12 00:13:38 +0530

    Terminating Game

--- script.js
+++ script.js
@@ -31,8 +31,11 @@ async function handleButtonClick(clickbtn) {
 
             if (arr[iteration] && arr[iteration] !== clickbtn.id) {
                 console.log("Wrong Move — Better Luck Next Time");
+                alert("Wrong Move — Better Luck Next Time");
                 score = 1;
                 iteration = 0;
+                let displayScore = document.getElementById("score");
+                displayScore.innerText = score-1;
                 arr = []; // Reset the sequence on wrong move
                 return;
             }
@@ -44,7 +47,7 @@ async function handleButtonClick(clickbtn) {
             iteration++;
 
             if (iteration === score) {
-                await new Promise(resolve => setTimeout(resolve, 1000)); // Pause for 1 second
+                await new Promise(resolve => setTimeout(resolve, 1000)); // Eat V-Star And Do Nothing ~ For a second🤞
 
                 await generateRandomClick();
 
